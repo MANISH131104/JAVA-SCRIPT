ec80817d0219355a828d1b050ef31ce094504c4f
manish131104 <manishkumar117694@gmail.com>
2026-07-10 23:24:31 +0530

    Your commit message

--- 13_functions.js
+++ 13_functions.js
@@ -41,4 +41,35 @@ function loginUserMessage(username = "sam"){
 }
 
 // console.log(loginUserMessage("Manish"))
-console.log(loginUserMessage("Manish"))
+// console.log(loginUserMessage("Manish"))
+
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200,400,500,2000))
+
+const user = {
+    username: "Manish",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArr = [200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArr));
+console.log(returnSecondValue([200,400,500,1000]));
