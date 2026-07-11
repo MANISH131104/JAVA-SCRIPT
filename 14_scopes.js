fe7a921e43d93bcc6239858837930b43aed11ca2
manish131104 <manishkumar117694@gmail.com>
2026-07-11 12:20:45 +0530

    Your commit message

--- 14_scopes.js
+++ 14_scopes.js
@@ -4,12 +4,57 @@ let a = 300
 if(true){
     let a = 10
     const b = 20
-    console.log("INNER: ", a);
+   // console.log("INNER: ", a);
 }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const userName = "Manish"
+
+    function two(){
+        const website = "youtube"
+        console.log(userName);
+    }
+    // console.log(website);
+
+    two()
+
+}
+
+// one()
+
+
+if (true) {
+    const username = "Manish"
+    if (username === "Manish") {
+        const website = " youtube"
+      //  console.log(username + website);
+    }
+   // console.log(website);
+}
+
+// console.log(userName);
+
+
+
+
+// +++++++++++ interesting ++++++++++
+
+console.log(addone(5))
+
+function addone(num){
+    return num+1
+}
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num+2
+}
 
 
