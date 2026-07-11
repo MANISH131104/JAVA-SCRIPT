0ea4ca17ab85a9e1d866d2df64a1b98591de193f
manish131104 <manishkumar117694@gmail.com>
2026-07-11 17:41:15 +0530

    Your commit message

---
+++ 16_IIFE.js
@@ -0,0 +1,10 @@
+// Immediately Involved Function Expressions (IIFE)
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+})('Manish')
